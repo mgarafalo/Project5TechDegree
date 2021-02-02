c50b3df4b16a98b23fa358b446065e78aeeb4d8a
mgarafalo <matt9194@gmail.com>
2021-02-02 18:35:36 -0500

    Final 3.0

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ document.getElementById('search').addEventListener("keyup", searchFilter);
 
 function searchFilter() {
     var searchInput = document.getElementById('search'); //sets input variabnle to the search box
-    var image = document.getElementsByTagName('img');
+    var image = document.getElementsByTagName('a');
     filter = searchInput.value.toLowerCase(); // sets filter to the value of whats being typed into the input box/search box
 
     for (var i = 0; i < 12; i++) { // Loop through the img tags and assigning the alt tags to variable altname 
-        var altName = image[i].getAttribute('alt'); // sets altText to the image in the loop at position i 
+        var altName = image[i].getAttribute('data-caption'); // sets altText to the image in the loop at position i 
         var altText = altName;
 
         if (altText.toLowerCase().includes(filter)) { // if the alt attr of an img tag contains the value of the search box text being input
